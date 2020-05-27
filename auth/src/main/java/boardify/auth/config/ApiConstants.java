32f9713f3238c539c25d6970e48f5aba66866518
Radu599 <30553913+Radu599@users.noreply.github.com>
2020-05-28 01:13:02 +0300

    refactor apiconstants

--- auth/src/main/java/boardify/auth/config/ApiConstants.java
+++ auth/src/main/java/boardify/auth/config/ApiConstants.java
@@ -1,12 +1,25 @@
 package boardify.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
 public class ApiConstants {
+
+    @Autowired
+    private static Environment environment;
+    private static String development = "development";
+
     private static final String HTTP = "http://";
 
-    private static final String IP = "192.168.99.100";
+    private static final String localhost = "localhost";
+    private static final String dockerIp = "192.168.99.100";
     private static final String PORT = ":8084";
-    // private static final String IP = "localhost";
 
-    private static final String USER_BASE_API = HTTP + IP + PORT + "/users";
+    private static final String USER_BASE_API = HTTP + findIp() + PORT + "/users";
+
+    public static String findIp(){
+        return  (environment.getActiveProfiles().equals(development))? localhost :(dockerIp);
+    }
+
     public static final String FIND_USER = USER_BASE_API + "/";
 }
