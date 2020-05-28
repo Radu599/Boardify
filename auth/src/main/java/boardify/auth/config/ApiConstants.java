8a451f01fe95edff8c307d4eef908137a5f9563b
Radu599 <30553913+Radu599@users.noreply.github.com>
2020-05-28 12:33:24 +0300

    bug fix auth

--- auth/src/main/java/boardify/auth/config/ApiConstants.java
+++ auth/src/main/java/boardify/auth/config/ApiConstants.java
@@ -1,25 +1,21 @@
 package boardify.auth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import java.util.Arrays;
 
 public class ApiConstants {
 
-    @Autowired
-    private static Environment environment;
-    private static String development = "development";
-
+    private static final String development = "development";
     private static final String HTTP = "http://";
-
     private static final String localhost = "localhost";
     private static final String dockerIp = "192.168.99.100";
     private static final String PORT = ":8084";
 
-    private static final String USER_BASE_API = HTTP + findIp() + PORT + "/users";
+    private static String USER_BASE_API_DEV = HTTP + localhost + PORT + "/users";
+    private static String USER_BASE_API_PROD = HTTP + dockerIp + PORT + "/users";
 
-    public static String findIp(){
-        return  (environment.getActiveProfiles().equals(development))? localhost :(dockerIp);
+    public static String getFindUser(String[] environment) {
+
+        boolean contains = Arrays.asList(environment).contains(development);
+        return contains ?USER_BASE_API_DEV:USER_BASE_API_PROD;
     }
-
-    public static final String FIND_USER = USER_BASE_API + "/";
 }
--- auth/src/main/java/boardify/auth/dao/impl/UserDaoJpa.java
+++ auth/src/main/java/boardify/auth/dao/impl/UserDaoJpa.java
@@ -5,13 +5,17 @@ import boardify.auth.model.BoardifyUser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import static boardify.auth.config.ApiConstants.FIND_USER;
+import static boardify.auth.config.ApiConstants.getFindUser;
 
 @Component
 public class UserDaoJpa implements UserDao {
+
+    @Autowired
+    private Environment environment;
 
     private final Logger logger = LogManager.getLogger(UserDaoJpa.class);
 
@@ -26,7 +30,9 @@ public class UserDaoJpa implements UserDao {
     public BoardifyUser findUser(String username) {
 
         logger.info("++++LOGGING findUser Dao");
-        BoardifyUser boardifyUser = restTemplate.getForObject(FIND_USER + username, BoardifyUser.class);
+        logger.info("Active profiles:" + environment.getActiveProfiles());
+        logger.info("Request user from " + getFindUser(environment.getActiveProfiles()));
+        BoardifyUser boardifyUser = restTemplate.getForObject(getFindUser(environment.getActiveProfiles()) + "/" + username, BoardifyUser.class);
         logger.info("User is:");
         logger.info(boardifyUser.toString());
         logger.info("++++SUCCESSFULLY LOGGING findUser Dao");
