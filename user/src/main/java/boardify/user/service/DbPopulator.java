80ce63b7f0785d098f4c98b53d8cf2ba59b661a5
Radu599 <30553913+Radu599@users.noreply.github.com>
2020-06-29 15:57:12 +0300

    Commit dupa predarea licentei

--- user/src/main/java/boardify/user/service/DbPopulator.java
+++ user/src/main/java/boardify/user/service/DbPopulator.java
@@ -1,4 +1,25 @@
 package boardify.user.service;
 
-public class DbPopulator {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+class CommandLineAppStartupRunner implements CommandLineRunner {
+
+    private Service service;
+
+    @Autowired
+    public CommandLineAppStartupRunner(Service service){
+
+        this.service = service;
+    }
+
+    @Override
+    public void run(String...args) throws Exception {
+
+        service.registerUser("x", "x");
+        service.registerUser("y", "y");
+        service.registerUser("z", "z");
+    }
 }
--- user/src/main/resources/db/changelog/changes/changelog_auth_production.sql
+++ user/src/main/resources/db/changelog/changes/changelog_auth_production.sql
@@ -1,4 +1,4 @@
--- changelog number: 1
+ -- changelog number: 1
 -- Antinie Radu creates table 'ROLES'
 show variables like 'lower_case_table_names';
 CREATE TABLE IF NOT EXISTS roles (
@@ -20,7 +20,3 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO `roles` (`ID`, `TYPE`) VALUES ('1', 'admin');
 INSERT INTO `roles` (`ID`, `TYPE`) VALUES ('2', 'normal');
 
-INSERT INTO `users` (`USER_ID`, `ROLE_ID`, `LOCATION`, `PASSWORD`) VALUES ('radu@yahoo.com', '1','Romania,Sibiu', 'radu');
-INSERT INTO `users` (`USER_ID`, `ROLE_ID`, `LOCATION`, `PASSWORD`) VALUES ('a@a.com', '2', 'France,Paris','a');
-INSERT INTO `users` (`USER_ID`, `ROLE_ID`, `LOCATION`, `PASSWORD`) VALUES ('b@b.com', '2', 'Romania,Cluj-Napoca','b');
-INSERT INTO `users` (`USER_ID`, `ROLE_ID`, `LOCATION`, `PASSWORD`) VALUES ('c@c.com', '2', 'Romania,Bucuresti','c');
